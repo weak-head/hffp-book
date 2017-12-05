bd86b1aba188b1eae2a45420be944c60a54685a0
Oleksandr Zinchenko <zinchenko@live.com>
2017-12-04 21:37:53 -0800

    Basic applicative overview

--- ch17-applicative/src/Lib.hs
+++ ch17-applicative/src/Lib.hs
@@ -1,6 +1,35 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE TypeApplications #-}
+
+module Lib where
+
+import Control.Applicative
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- class Functor f => Applicative f where
+--   -- identity
+--   pure :: a -> f a
+--   -- apply, tie fighter
+--   (<*>) :: f (a -> b) -> f a -> f b
+
+-- fmap
+-- <$> :: Functor f => (a -> b) -> f a -> f b
+
+-- <*> :: Applicative f => f (a -> b) -> f a -> f b
+
+
+-- Control.Applicative
+-- liftA :: Applicative f => (a -> b) -> f a -> f b
+-- liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+-- liftA3 :: Applicative f => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
+
+
+-- fmap f x = pure f <*> x
+lf1 = fmap (+1) [1, 2, 3]
+lf2 = pure (+1) <*> [1, 2, 3]
+
+lfp1 = pure 1 :: [Int]        -- [1]
+lfp2 = pure 1 :: Maybe Int    -- Just 1
+lfp3 = pure 1 :: Either a Int -- Right 1
+lfp4 = pure 1 :: ([a], Int)   -- ([], 1)
