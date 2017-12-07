490b99786e74b098ad8cdbdc0cdc0a6bfc4f98d0
Oleksandr Zinchenko <zinchenko@live.com>
2017-12-07 00:00:59 -0800

    Applicative: list instance, ads

--- ch17-applicative/test/ListApplicativeSpec.hs
+++ ch17-applicative/test/ListApplicativeSpec.hs
@@ -17,7 +17,6 @@ instance Applicative List where
   (<*>) _ Nil = Nil
   (<*>) (Cons f fs) xs = conc (f <$> xs) (fs <*> xs)
 
-
 conc :: List a -> List a -> List a
 conc Nil v = v
 conc (Cons v xs) ys = Cons v (conc xs ys)
@@ -27,3 +26,15 @@ f = Cons (+1) (Cons (*2) Nil)
 v = Cons 1 (Cons 2 Nil)
 ap = f <*> v
 -- Cons 2 (Cons 3 (Cons 2 (Cons 4 Nil)))
+
+---- -- --
+
+fold :: (a -> b -> b) -> b -> List a -> b
+fold _ v Nil = v
+fold f v (Cons x xs) = f x (fold f v xs)
+
+flat :: List (List a) -> List a
+flat = fold conc Nil
+
+flatMap :: (a -> List b) -> List a -> List b
+flatMap f = flat . fmap f
