5bb65f94a24a7cd1bcea250e15504ab9899248c4
Oleksandr Zinchenko <zinchenko@live.com>
2017-12-06 23:21:10 -0800

    Applicative: list instance

---
+++ ch17-applicative/test/ListApplicativeSpec.hs
@@ -0,0 +1,29 @@
+module ListApplicativeSpec where
+
+data List a =
+    Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+instance Applicative List where
+  -- a -> f a
+  pure x = Cons x Nil
+  -- f (a -> b) -> f a -> f b
+  (<*>) Nil _ = Nil
+  (<*>) _ Nil = Nil
+  (<*>) (Cons f fs) xs = conc (f <$> xs) (fs <*> xs)
+
+
+conc :: List a -> List a -> List a
+conc Nil v = v
+conc (Cons v xs) ys = Cons v (conc xs ys)
+
+
+f = Cons (+1) (Cons (*2) Nil)
+v = Cons 1 (Cons 2 Nil)
+ap = f <*> v
+-- Cons 2 (Cons 3 (Cons 2 (Cons 4 Nil)))
