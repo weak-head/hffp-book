f849d2db4f9b90f04ca4e4067e711bde95f52225
Oleksandr Zinchenko <zinchenko@live.com>
2017-12-06 22:17:47 -0800

    Applicative: law testing using 'checkers' lib

--- ch17-applicative/test/Spec.hs
+++ ch17-applicative/test/Spec.hs
@@ -10,3 +10,33 @@ spec = parallel $ do
   describe "Applicative laws" $ do
     it "Identity" $ do
       2 `shouldBe` 2
+
+-- applicative
+--   :: ( Show a
+--      , Show (m a)
+--      , Show (m (a -> b))
+--      , Show (m (b -> c))
+--      , Applicative m
+--      , CoArbitrary a
+--      , EqProp (m a)
+--      , EqProp (m b)
+--      , EqProp (m c)
+--      , Arbitrary a
+--      , Arbitrary b
+--      , Arbitrary (m a)
+--      , Arbitrary (m (a -> b))
+--      , Arbitrary (m (b -> c)))
+--   => m (a, b, c) -> TestBatch
+
+
+jqmain = quickBatch $ applicative xs
+  where xs = [("b", "w", 1 :: Int)]
+
+
+-- The value we are passing in, is used
+-- just to know a type
+type SSI = (String, String, Int)
+momain = quickBatch $ applicative tr
+  where
+    tr :: [SSI]
+    tr = undefined
