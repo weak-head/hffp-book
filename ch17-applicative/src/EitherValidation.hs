b847ce43686e25bef51013efaa32b7bbf424cb10
Oleksandr Zinchenko <zinchenko@live.com>
2017-12-11 22:15:05 -0800

    Applicative: validation

--- ch17-applicative/src/EitherValidation.hs
+++ ch17-applicative/src/EitherValidation.hs
@@ -1,5 +1,7 @@
 module EitherValidation where
 
+import Data.Monoid
+
 -- f ~ Either e
 
 type E = Either
@@ -23,3 +25,56 @@ type E = Either
 --
 -- > Left "sad" <*> Left "sadface.png"
 -- Left "sad"
+
+
+data Validation err a =
+    Failure err
+  | Success a
+  deriving (Eq, Show)
+
+-- natural tranformations:
+
+validToEither :: Validation e a -> Either e a
+validToEither (Failure err) = Left err
+validToEither (Success a)   = Right a
+
+eitherToValid :: Either e a -> Validation e a
+eitherToValid (Left err) = Failure err
+eitherToValid (Right a)  = Success a
+
+-- eitherToValid . validToEither == id
+-- validToEither . eitherToValid == id
+
+data Errors =
+    DevidedByZero
+  | StackOverflow
+  | MooglesChewedWires
+  deriving (Eq, Show)
+
+success =   Success (+1)
+        <*> Success 1
+-- success = Success 2
+
+failure =   Success (+1)
+        <*> Failure [StackOverflow]
+-- failure = Failure [StackOverflow]
+
+failure' =   Failure [StackOverflow]
+         <*> Success (+1)
+-- failure' = Failure [StackOverflow]
+
+failures =
+      Failure [MooglesChewedWires]
+  <*> Failure [StackOverflow]
+-- failures = Failure [MooglesChewedWires, StackOverflow]
+
+instance Functor (Validation err) where
+  fmap _ (Failure err) = Failure err
+  fmap f (Success a)   = Success (f a)
+
+instance Monoid err => Applicative (Validation err) where
+  pure a = Success a
+  (<*>) (Failure f) (Failure v) = Failure $ f <> v
+  (<*>) (Failure f) _           = Failure f
+  (<*>) _           (Failure v) = Failure v
+  (<*>) (Success f) (Success v) = Success (f v)
