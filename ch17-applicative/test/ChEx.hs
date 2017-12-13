4291996b37c07296ddeed9862d0da7dc2d9f77e2
Oleksandr Zinchenko <zinchenko@live.com>
2017-12-12 22:30:38 -0800

    Applicative: Three' instance

--- ch17-applicative/test/ChEx.hs
+++ ch17-applicative/test/ChEx.hs
@@ -110,3 +110,31 @@ threeChecker = quickBatch $ applicative a
     a = undefined
 
 ----------------------------------------------------------------------
+
+data Three' a b =
+  Three' a b b
+  deriving (Eq, Show)
+
+instance Functor (Three' a) where
+  fmap f (Three' a b c) = Three' a (f b) (f c)
+
+instance Monoid a => Applicative (Three' a) where
+  pure x = Three' mempty x x
+  (<*>) (Three' s f1 f2) (Three' s' v1 v2) = Three' (s <> s') (f1 v1) (f2 v2)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return $ Three' a b c
+
+instance (Eq a, Eq b) => EqProp (Three' a b) where
+  (=-=) = eq
+
+threeCheck = quickBatch $ applicative a
+  where
+    a :: Three' String (Int, String, Bool)
+    a = undefined
+
+----------------------------------------------------------------------
