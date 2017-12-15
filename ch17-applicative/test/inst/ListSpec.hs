60d81dce8b012a74499042e0cddf8f346246dbd8
Oleksandr Zinchenko <zinchenko@live.com>
2017-12-14 23:28:25 -0800

    applicative: basic list implementation

--- ch17-applicative/test/inst/ListSpec.hs
+++ ch17-applicative/test/inst/ListSpec.hs
@@ -3,15 +3,37 @@ module ListSpec where
 import Test.QuickCheck
 import Test.QuickCheck.Checkers
 
+-- | Defines a linked list
 data List a =
     Nil
   | Cons a (List a)
   deriving (Show, Eq)
 
+-- | Right associative fold of a 'List'
+foldr' :: (a -> b -> b) -> b -> List a -> b
+foldr' _ v Nil        = v
+foldr' f v (Cons h t) = f h (foldr' f v t)
+
+-- | Append two 'List'
+con' :: List a -> List a -> List a
+con' Nil xs = xs
+con' xs Nil = xs
+con' (Cons h t) xs = Cons h (con' t xs)
+
+-- | The concatenation of all elements of a 'List' of 'List'
+concat' :: List (List a) -> List a
+concat' = foldr' con' Nil
+
+-- | Map a function over all the elements of a 'List' and concatenate the resulting 'List'
+concatMap' :: (a -> List b) -> List a -> List b
+concatMap' f = concat' . fmap f
+
+-- | The 'List' functor
 instance Functor List where
   fmap _ Nil = Nil
   fmap f (Cons a l) = Cons (f a) (fmap f l)
 
+-- | The 'List' functor with application
 instance Applicative List where
   pure x      = Cons x Nil
-  (<*>) fs xs = undefined
+  (<*>) fs xs = concatMap' ($xs) (fmap fmap fs)
