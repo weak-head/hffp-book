874344bb5ba0cdaa5d2ff26074c46415c66759ec
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2017-12-21 22:46:17 -0800

    Monad: simple fmap example

--- ch18-monad/src/DoSyntax.hs
+++ ch18-monad/src/DoSyntax.hs
@@ -1,5 +1,7 @@
 module DoSyntax where
 
+import Data.Maybe
+
 -- (*>) :: Applicative f => f a -> f b -> f b
 -- (>>) :: Monad m       => m a -> m b -> m b
 
@@ -31,3 +33,43 @@ binding = do
 binding' :: IO ()
 binding' =
   getLine >>= putStrLn
+
+-----
+
+-- This won't work as expected:
+-- > putStrLn <$> getLine
+--
+-- > :t putStrLn <$> getLine
+-- putStrLn <$> getLine :: IO (IO ())
+--
+-- > :t getLine
+-- getLine :: IO String
+--
+-- > :t putStrLn
+-- putStrLn :: String -> IO ()
+--
+--
+-- <$> :: Functor f => (a -> b) -> f a -> f b
+--              ( a   -> b   )
+-- putStrLn :: Strign -> IO ()
+--
+
+f :: Functor f => f String -> f (IO ())
+f x = putStrLn <$> x
+
+g :: (String -> b) -> IO b
+g x = x <$> getLine
+
+-- putStrLn <$> getLine :: IO (IO ())
+
+--
+fj = fromJust $ putStrLn <$> Just "abc"
+fg = (++ "abc") <$> getLine
+
+
+--  [1] [2] [3]
+h :: IO (IO ())
+h = putStrLn <$> getLine
+-- [1] -> effect 'getLine' must perform to get a String
+-- [2] -> effect that would be performed if 'putStrLn' was evaluated
+-- [3] -> return of the 'putStrLn'
