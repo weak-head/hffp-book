4e0cf2d86eb9b43d9ac69a501b3504da821ffc0e
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2017-12-23 11:00:36 -0800

    Monad: Either validation and a simple coproduct instance

--- ch18-monad/src/SimpleMonadExamples.hs
+++ ch18-monad/src/SimpleMonadExamples.hs
@@ -162,7 +162,7 @@ doSmtCt n = do
   c <- h' b
   pure (a, b, c)
 
-----------------------------------------------------------------------
+----
 
 -- instance Monad Maybe where
 --   return x       = Just x
@@ -175,4 +175,97 @@ doSmtCt n = do
 -- > Just 1 >>= undefined
 -- *** Exception: Prelude.undefined
 
+----------- Either ---------------------------------------------------
+
+--
+-- m ~ Either e
+-- (>>=) :: Monad m => m        a -> (a -> m        b) -> m        b
+-- (>>=) ::            Either e a -> (a -> Either e b) -> Either e b
+--
+--
+-- return :: Monad m => a -> m        a
+-- return ::            a -> Either e a
+
+type Founded = Int
+type Coders  = Int
+
+data SoftwareShop =
+  Shop
+  { founded     :: Founded
+  , programmers :: Coders
+  } deriving (Eq, Show)
+
+data FoundedError =
+    NegativeYears Founded
+  | TooManyYears Founded
+  | NegativeCoders Coders
+  | TooManyCoders Coders
+  | TooManyCodersForYears Founded Coders
+  deriving (Eq, Show)
+
+validateFounded :: Int -> Either FoundedError Founded
+validateFounded n
+  | n < 0     = Left $ NegativeYears n
+  | n > 500   = Left $ TooManyYears n
+  | otherwise = Right n
+
+validateCoders :: Int -> Either FoundedError Coders
+validateCoders n
+  | n < 0 = Left $ NegativeCoders n
+  | n > 5000 = Left $ TooManyCoders n
+  | otherwise = Right n
+
+-- Unfortunatelly we cannot make an accumulative Monad for
+-- Validation. In Monad later values can depend on previous ones.
+mkSoftware :: Int -> Int -> Either FoundedError SoftwareShop
+mkSoftware years coders = do
+  founded     <- validateFounded years
+  programmers <- validateCoders coders
+  if programmers > div founded 10
+    then Left $ TooManyCodersForYears founded programmers
+    else Right $ Shop founded programmers
+
+
+-- import Control.Monad (ap)
+--
+-- (<*>) == ap
+--
+-- (<*>) :: Applicative f => f (a -> b) -> f a -> f b
+-- ap    :: Monad m       => m (a -> b) -> m a -> m b
+--
+
+ap' :: (Monad m) => m (a -> b) -> m a -> m b
+ap' m m' = do
+  f <- m
+  v <- m'
+  return (f v)
+
+ap'' :: (Monad m) => m (a -> b) -> m a -> m b
+ap'' m m' =
+  m >>=
+  \f ->
+    m' >>=
+    \v ->
+      return $ f v
+
 ----------------------------------------------------------------------
+
+data Sum a b =
+    First a
+  | Second b
+  deriving (Show, Eq)
+
+instance Functor (Sum a) where
+  fmap _ (First a)  = First a
+  fmap f (Second b) = Second (f b)
+
+instance Applicative (Sum a) where
+  pure = Second
+  (<*>) (First a) _ = First a
+  (<*>) _ (First a) = First a
+  (<*>) (Second f) (Second v) = Second (f v)
+
+instance Monad (Sum a) where
+  return = Second
+  (>>=) (First a) _  = First a
+  (>>=) (Second v) f = f v
