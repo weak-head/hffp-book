5da6366c02bde6528a9ebdea5a2904231e170b62
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2017-12-26 11:22:31 -0800

    Monad: Either instance and test batch

--- ch18-monad/test/ChEx.hs
+++ ch18-monad/test/ChEx.hs
@@ -15,6 +15,10 @@ spec = parallel $ do
     testBatch (functor (undefined :: Nope (Int, Int, Int)))
     testBatch (applicative (undefined :: Nope (Int, Bool, String)))
     testBatch (monad (undefined :: Nope (Int, String, String)))
+  describe "MEither" $ do
+    testBatch (functor (undefined :: MEither Int (Int, Int, Int)))
+    testBatch (applicative (undefined :: MEither String (Int, Bool, String)))
+    testBatch (monad (undefined :: MEither Bool (Int, String, String)))
 
 
 --- Nope instance ----------------------------------------------------
@@ -39,3 +43,33 @@ instance EqProp (Nope a) where
 
 instance Arbitrary (Nope a) where
   arbitrary = return NopeDotJpg
+
+--- Either instance --------------------------------------------------
+
+data MEither b a =
+    MLeft a
+  | MRight b
+  deriving (Show, Eq)
+
+instance Functor (MEither b) where
+  fmap _ (MRight b) = MRight b
+  fmap f (MLeft a)  = MLeft (f a)
+
+instance Applicative (MEither b) where
+  pure = MLeft
+  (<*>) (MRight b) _ = MRight b
+  (<*>) _ (MRight b) = MRight b
+  (<*>) (MLeft f) (MLeft v) = MLeft (f v)
+
+instance Monad (MEither b) where
+  return = pure
+  (>>=) (MRight b) _ = MRight b
+  (>>=) (MLeft a) f = f a
+
+instance (Eq a, Eq b) => EqProp (MEither b a) where
+  (=-=) = eq
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (MEither b a) where
+  arbitrary =
+    frequency [ (1, MLeft <$> arbitrary)
+              , (1, MRight <$> arbitrary) ]
