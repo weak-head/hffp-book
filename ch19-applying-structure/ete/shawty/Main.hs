0b5403058fac4245ad1e09f685cd114cb778f692
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2017-12-27 11:22:05 -0800

    extending shawty example

--- ch19-applying-structure/ete/shawty/Main.hs
+++ ch19-applying-structure/ete/shawty/Main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-modele Main where
+module Main where
 
 import           Control.Monad (replicateM)
 import           Control.Monad.IO.Class (liftIO)
@@ -13,4 +13,72 @@ import qualified System.Random as SR
 import           Web.Scotty
 
 
-main = putStrLn "works"
+alphaNum :: String
+aplhaNum = ['A'..'Z'] ++ ['0'..'9']
+
+randomElement :: String -> IO Char
+randomElement xs = do
+  let maxIndex = length xs - 1
+  randomDigit <- SR.randomRIO (0, maxIndex)
+  return (xs !! randomDigit)
+
+shortyGen :: IO [Char]
+shortyGen =
+  replicateM 7 $ randomElement alphaNum
+
+-- replicateM 3 "abc"
+-- replicate  3 "abc"
+
+
+
+saveURI :: R.Connection
+        -> BC.ByteString
+        -> BC.ByteString
+        -> IO (Either R.Reply R.Status)
+saveURI conn shortURI uri =
+  R.runRedis conn $ R.set shortURI uri
+
+
+getURI :: R.Connection
+       -> BC.ByteString
+       -> IO (Either R.Reply
+              (Maybe BC.ByteString))
+getURI conn shortURI =
+  R.runRedis conn $ R.get shortURI
+
+
+
+linkShorty :: Strign -> String
+linkShorty shorty =
+  concat
+  [ "<a href=\""
+  , shorty
+  , "\">Copy and paste your short URL</a>"]
+
+
+shortyCreated :: Show a
+              => a
+              -> String
+              -> TL.Text
+shortyCreated resp shawty =
+  TL.concat [ TL.pack (show resp)
+            , " shorty is: "
+            , TL.pack (linkShorty shawty)
+            ]
+
+
+shortyAintUri :: TL.Text -> TL.Text
+shortyAintUri uri =
+  TL.concat
+    [ uri
+    , " wasn't a url,"
+    , " did you forget http://?"
+    ]
+
+shortyFound :: TL.Text -> TL.Text
+shortyFound tbs =
+  TL.concat
+    [ "<a href=\""
+    , tbs, "\">"
+    , tbs, "</a>"
+    ]
