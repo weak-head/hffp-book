7195fe70e8556c1a4879aacf7dcb0d0c93062ada
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2017-12-30 12:44:09 -0800

    Traversable: sequence & fmap composition

---
+++ ch21-traversable/src/Sequence.hs
@@ -0,0 +1,29 @@
+module Sequence where
+
+
+sf :: (Monad m, Traversable t) => (a -> m b) -> t a -> m (t b)
+sf = (sequence .) . fmap
+
+
+{-
+
+Lets follow the types:
+
+ (.)  :: (b -> c) -> (a -> b) -> a -> c
+
+ fmap :: (a -> b) -> f a -> f b
+
+ sequence :: (Monad m, Traversable t)
+          => t (m a) -> m (t a)
+
+--
+
+ (sequence .) :: (Monad m, Traversable t)
+              => (a -> t (m b)) -> a -> m (t b)
+
+ ((sequence .) .) :: (Monad m, Traversable t)
+                  => (a -> b -> t (m c)) -> a -> b -> m (t c)
+
+ ((sequence .) . fmap) :: (Monad m, Traversable t)
+                       => (a -> m b) -> t a -> m (t b)
+-}
