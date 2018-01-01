93c3ee8d8f4f25160d9a8707e5d35c8a644b9a92
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-01 13:23:26 -0800

    Traversable: Constant

--- ch21-traversable/src/ChEx.hs
+++ ch21-traversable/src/ChEx.hs
@@ -27,3 +27,28 @@ instance Eq a => EqProp (Identity a) where
 identityQB = do
   quickBatch (functor (undefined :: Identity (Int, Int, [Int])))
   quickBatch (traversable (undefined :: Identity (Int, Int, [Int])))
+
+--- Constant ---------------------------------------------------------
+
+newtype Constant a b =
+  Constant { getConstant :: a }
+  deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant a) = Constant a
+
+instance Foldable (Constant a) where
+  foldr _ d _ = d
+
+instance Traversable (Constant a) where
+  traverse f (Constant a) = pure $ Constant a
+
+instance Arbitrary a => Arbitrary (Constant a b) where
+  arbitrary = Constant <$> arbitrary
+
+instance (Eq a) => EqProp (Constant a b) where
+  (=-=) = eq
+
+constantQB = do
+  quickBatch (functor (undefined :: Constant Int (Int, Int, [Int])))
+  quickBatch (traversable (undefined :: Constant Int (Int, Int, [Int])))
