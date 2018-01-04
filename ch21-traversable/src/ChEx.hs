43209a8a38af8212cd4456154865966628d8e19f
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-03 23:13:30 -0800

    foldable and traversable Tree instance

--- ch21-traversable/src/ChEx.hs
+++ ch21-traversable/src/ChEx.hs
@@ -254,3 +254,42 @@ sSm = sample' (arbitrary :: Gen (S [] Int))
 sQB = do
   quickBatch (functor (undefined :: S Maybe (Int, Bool, [String])))
   quickBatch (traversable (undefined :: S Maybe (Int, Bool, [String])))
+
+--- Tree -------------------------------------------------------------
+
+data Tree a =
+    Empty
+  | Leaf a
+  | Node (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+instance Functor Tree where
+  fmap _ Empty        = Empty
+  fmap f (Leaf a)     = Leaf $ f a
+  fmap f (Node l a r) = Node (f <$> l) (f a) (f <$> r)
+
+instance Foldable Tree where
+  foldMap _ Empty        = mempty
+  foldMap f (Leaf a)     = f a
+  foldMap f (Node l a r) = foldMap f l <> f a <> foldMap f r
+
+instance Traversable Tree where
+  traverse _ Empty = pure Empty
+  traverse f (Leaf a) = Leaf <$> f a
+  traverse f (Node l a r) = liftA3 Node (traverse f l) (f a) (traverse f r)
+
+instance Eq a => EqProp (Tree a) where
+  (=-=) = eq
+
+instance Arbitrary a => Arbitrary (Tree a) where
+  arbitrary =
+    frequency [ (1, return Empty)
+              , (2, Leaf <$> arbitrary)
+              , (2, liftA3 Node arbitrary arbitrary arbitrary)
+              ]
+
+treeSm = sample' (arbitrary :: Gen (Tree Int))
+
+treeQB = do
+  quickBatch (functor (undefined :: Tree (Int, Bool, [String])))
+  quickBatch (traversable (undefined :: Tree (Int, Bool, [String])))
