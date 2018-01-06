c3bb9b2b54f52cec4f4272ef248590eec843a03c
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-05 16:30:32 -0800

    Monoid, Monad, Traversable instances for List

--- ch21-traversable/src/Sand/List.hs
+++ ch21-traversable/src/Sand/List.hs
@@ -8,16 +8,34 @@ data List a =
   deriving (Show, Eq)
 
 infixr 5 ++!
-(++!) :: List a -> List a -> List a
-(++!) = undefined
 
+(++!) :: List a -> List a -> List a
+Nil         ++! xs = xs
+(Cons x xs) ++! ys = Cons x (xs ++! ys)
+
+-- join
 concat_ :: List (List a) -> List a
-concat_ = undefined
+concat_ = foldr_ (++!) Nil
+
+instance Monoid_ (List a) where
+  mempty_  = Nil
+  mappend_ = (++!)
 
 instance Functor_ List where
   fmap_ _ Nil         = Nil
   fmap_ f (Cons x xs) = Cons (f x) (fmap_ f xs)
 
+instance Applicative_ List where
+  pure_ x    = Cons x Nil
+  fs <*>! xs = concat_ $ fmap_ ($xs) (fmap_ fmap_ fs)
+
+instance Monad_ List where
+  return_   = pure_
+  xs >>=! f = concat_ $ fmap_ f xs
+
 instance Foldable_ List where
   foldr_ _ d Nil         = d
   foldr_ f d (Cons x xs) = f x (foldr_ f d xs)
+
+instance Traversable_ List where
+  sequenceA_ = foldr_ (\v a -> fmap_ Cons v <*>! a) (pure_ Nil)
