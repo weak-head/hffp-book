2da2b5d089ad1322b3fa89249de3eb1392baf2f9
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-06 19:13:00 -0800

    functor and foldable for tree

---
+++ ch21-traversable/src/Sand/Tree.hs
@@ -0,0 +1,26 @@
+module Sand.Tree where
+
+import Sand.ClassDef
+
+data Tree a =
+    Empty
+  | Leaf a
+  | Node (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+instance Functor_ Tree where
+  fmap_ _ Empty        = Empty
+  fmap_ f (Leaf a)     = Leaf (f a)
+  fmap_ f (Node l v r) = Node (fmap_ f l) (f v) (fmap_ f r)
+
+instance Foldable_ Tree where
+  foldMap_ _ Empty        = mempty_
+  foldMap_ f (Leaf a)     = f a
+  foldMap_ f (Node l v r) = foldMap_ f l `mappend_` f v `mappend_` foldMap_ f r
+
+  foldr_ _ d Empty    = d
+  foldr_ f d (Leaf v) = f v d
+  foldr_ f d (Node l v r) =
+    let d'  = foldr_ f d  l
+        d'' = foldr_ f d' r
+    in f v d''
