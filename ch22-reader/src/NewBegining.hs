2451265da938bf99e15614fc04746c954e099d96
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-07 17:30:02 -0800

    exposing Reader idea on example of simple math functions

--- ch22-reader/src/NewBegining.hs
+++ ch22-reader/src/NewBegining.hs
@@ -1,1 +1,30 @@
 module NewBegining where
+
+import Control.Applicative
+
+boop :: Num a => a -> a
+boop = (*2)
+
+doop :: Num a => a -> a
+doop = (+10)
+
+
+bip :: Num a => a -> a
+bip = boop . doop -- \a -> boop (doop a)
+
+bloop :: Num a => a -> a
+bloop = fmap boop doop -- \a -> boop (doop a)
+
+bbop :: Num a => a -> a
+bbop = (+) <$> boop <*> doop
+-- :t (<*>) :: (a -> a -> b) -> (a -> a) -> (a -> b)
+
+duwop :: Num a => a -> a
+duwop = liftA2 (+) boop doop
+-- \a -> (boop a) + (doop a)
+
+boopDoop :: Num a => a -> a
+boopDoop = do
+  a <- boop
+  b <- doop
+  return (a + b)
