73cfcd29efa1a72e0e938c22b2c81b3fef96114e
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-10 23:49:28 -0800

    State example

---
+++ ch23-state/src/FizzBuzzState.hs
@@ -0,0 +1,25 @@
+module FizzBuzzState where
+
+import Control.Monad
+import Control.Monad.Trans.State
+
+fizzBuzz n | n `mod` 15 == 0 = "FizzBuzz"
+           | n `mod` 5  == 0 = "Buzz"
+           | n `mod` 3  == 0 = "Fizz"
+           | otherwise       = show n
+
+fizzBuzzMain = mapM_ (putStrLn . fizzBuzz) [1..100]
+
+--- Using State ------------------------------------------------------
+
+fizzbuzzList :: [Integer] -> [String]
+fizzbuzzList list = execState (mapM_ addResult list) []
+
+addResult :: Integer -> State [String] ()
+addResult n = do
+  xs <- get
+  let result = fizzBuzz n
+  put (result : xs)
+
+main :: IO ()
+main = mapM_ putStrLn $ reverse $ fizzbuzzList [1..100]
