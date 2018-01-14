cf99e304646a7ac143f0c01f9874195d0e824e16
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-14 15:18:18 -0800

    skeleton for parsing ini files

---
+++ ch24-parser-combinators/src/ParsingIni.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module ParsingIni where
+
+import Control.Applicative
+import Data.ByteString (ByteString)
+import Data.Char (isAlpha)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Text (Text)
+import qualified Data.Text.IO as TIO
+import Test.Hspec
+import Text.RawString.QQ
+import Text.Trifecta
