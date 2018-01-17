013cc1cd5f7ff74df869afa4db1dc946ff057d56
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-17 13:57:36 -0800

    parser-combinators: a few additional tokenization examples

--- ch24-parser-combinators/src/CharAndToken.hs
+++ ch24-parser-combinators/src/CharAndToken.hs
@@ -49,3 +49,55 @@ t4 = parseString (token (some (token decimal))) mempty ts
 
 t5 = parseString (some integer) mempty "1\n2\n 3\n"
 -- Success [1,2,3]
+
+----------------------------------------------------------------------
+
+p' :: Parser [Integer]
+p' = some $ do
+  i <- token (some digit)
+  return (read i)
+
+ps = "1\n2\n3"
+
+pc1 = parseString p' mempty ps
+-- Success [1,2,3]
+
+pc2 = parseString (token (some digit)) mempty ps
+-- Success "1"
+
+pc3 = parseString (some (token (some digit))) mempty ps
+-- Success ["1", "2", "3"]
+
+----------------------------------------------------------------------
+
+tknWhole :: Parser Char
+tknWhole = token $ char 'a' >> char 'b'
+
+tc1 = parseString tknWhole mempty "a b"
+-- Failure
+
+tc2 = parseString tknWhole mempty "ab ab"
+-- Success 'b'
+
+tc3 = parseString (some tknWhole) mempty "ab ab"
+-- Success "bb"
+
+
+tknCharA :: Parser Char
+tknCharA = token (char 'a') >> char 'b'
+
+ta1 = parseString tknCharA mempty "a b"
+-- Success 'b'
+
+ta2 = parseString (some tknCharA) mempty "a ba b"
+-- Success "bb"
+
+ta3 = parseString (some tknCharA) mempty "a b a b"
+-- Success "b"
+
+
+tknBoth :: Parser Char
+tknBoth = token (char 'a') >> token (char 'b')
+
+tb1 = parseString (some tknBoth) mempty "a b a b"
+-- Success "bb"
