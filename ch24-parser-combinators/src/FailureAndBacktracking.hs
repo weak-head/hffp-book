253421e4b3b1d64aadb77ca4b30a70199ae09045
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-17 17:09:42 -0800

    parser-combinators: failure and backtracking

---
+++ ch24-parser-combinators/src/FailureAndBacktracking.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module FailureAndBacktracking where
+
+import           Control.Applicative
+import           Data.Attoparsec.ByteString (parseOnly)
+import qualified Data.Attoparsec.ByteString as A
+import           Data.ByteString (ByteString)
+import           Text.Parsec (Parsec, parseTest)
+import           Text.Trifecta hiding (parseTest)
+
+-- trifecta
+trifP :: Show a => Parser a -> String -> IO ()
+trifP p i = print $ parseString p mempty i
+
+-- parsec
+parsecP :: Show a => Parsec String () a -> String -> IO ()
+parsecP = parseTest
+
+-- attoparsec
+attoP :: Show a => A.Parser a -> ByteString -> IO ()
+attoP p i = print $ parseOnly p i
+
+----------
+
+nobackParse :: (Monad f, CharParsing f) => f Char
+nobackParse = (char '1' >> char '2')
+              <|> char '3'
+
+tryParse :: (Monad f, CharParsing f) => f Char
+tryParse = try (char '1' >> char '2')
+           <|> char '3'
+
+----------
+
+main :: IO ()
+main = do
+  -- trifecta
+  trifP nobackParse "13"
+  trifP tryParse "13"
+
+  -- parsec
+  parsecP nobackParse "13"
+  parsecP tryParse "13"
+
+  -- attoparsec
+  attoP nobackParse "13"
+  attoP tryParse "13"
