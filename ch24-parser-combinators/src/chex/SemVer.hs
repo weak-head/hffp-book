2098ea3fe37b9660664fd16f301623e4bce56d6e
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-18 21:24:06 -0800

    parser-combinators: partially working SemVer

--- ch24-parser-combinators/src/chex/SemVer.hs
+++ ch24-parser-combinators/src/chex/SemVer.hs
@@ -4,6 +4,9 @@
 
 module SemVer where
 
+import Control.Applicative
+import Text.Trifecta
+
 data NumberOrString =
     NOSS String
   | NOSI Integer
@@ -17,7 +20,7 @@ type Metadata = [NumberOrString]
 
 -- | Defines the semantic version.
 --
--- Semanti version has the following structure:
+-- Semantic version has the following structure:
 --  1    .    1    .    5   -  y.4.x.7   +  exp.511f2.2fs
 --  |         |         |         |               |
 -- Major    Minor     Patch    Release         Metadata
@@ -28,9 +31,50 @@ type Metadata = [NumberOrString]
 --  * 2.7.3-x.7.z.92+sha.258
 --
 data SemVer =
-  SemVer { major :: Major
-         , minor :: Minor
-         , patch :: Patch
-         , release :: Release
+  SemVer { major    :: Major
+         , minor    :: Minor
+         , patch    :: Patch
+         , release  :: Release
          , metadata :: Metadata
          } deriving (Show, Eq)
+
+parseNos :: Parser NumberOrString
+parseNos =
+  skipMany (oneOf ".") >>
+  (NOSI <$> integer) <|> (NOSS <$> some letter)
+
+parseRelease :: Parser Release
+parseRelease = do
+  _ <- char '-'
+  some parseNos
+
+parseMetadata :: Parser Metadata
+parseMetadata = do
+  _ <- char '+'
+  some parseNos
+
+parseSemVer :: Parser SemVer
+parseSemVer = do
+  mj <- integer
+  _ <- char '.'
+  mn <- integer
+  _ <- char '.'
+  pt <- integer
+  try (liftA2 (SemVer mj mn pt) (try parseRelease) (try parseMetadata))
+    <|> try (liftA2 (SemVer mj mn pt) (try parseRelease) (pure []))
+    <|> try (SemVer mj mn pt [] <$> (try parseMetadata))
+    <|> (return $ SemVer mj mn pt [] [])
+
+
+
+main :: IO ()
+main = do
+  let parse = parseString parseSemVer mempty
+  print $ parse "2.1.1"
+  print $ parse "1.2.3-x.7.z.92"
+  print $ parse "1.7.4+20130313144700"
+  print $ parse "1.4.7+exp.sha.5114f85"
+  print $ parse "0.9.7-beta+exp.sha.5114f85"
+  print $ parse "3.1.7-x.17.92+e2"
+
+  -- SemVer 2 1 1 [] [] > SemVer 2 1 0 [] []
