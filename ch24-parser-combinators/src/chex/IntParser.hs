160c5bfd5743966b2c70b6443463483d86c577da
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-22 11:18:49 -0800

    parser-combinators: negative integer parsing

--- ch24-parser-combinators/src/chex/IntParser.hs
+++ ch24-parser-combinators/src/chex/IntParser.hs
@@ -1,5 +1,6 @@
 module IntParser where
 
+import Control.Applicative
 import Data.Foldable (foldl')
 import Data.Char (digitToInt)
 import Text.Trifecta
@@ -12,9 +13,11 @@ parseDigit :: Parser Char
 parseDigit = oneOf "0123456789"
 
 base10Int :: Parser Int
-base10Int = read <$> some parseDigit
--- to be completely honest here, we need to use
--- strToInt of 'read'
+base10Int = do
+  v <- try (const (-1) <$> char '-') <|> pure 1
+  (* v) . read <$> some parseDigit
+-- to follow the exercise requirements, we need to use
+-- strToInt instead of 'read' here
 
 main :: IO ()
 main = do
@@ -27,3 +30,7 @@ main = do
   print $ pi "123"
   print $ pi "123abc"
   print $ pi "abc"
+  ---
+  print $ pi "-123"
+  print $ pi "-123abc"
+  print $ pi "-abc"
