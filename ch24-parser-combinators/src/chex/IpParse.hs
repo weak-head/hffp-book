d9e610b0ad51e6bc806de9e7ea716a19f5366c0c
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-29 14:14:53 -0800

    parser-combinators: IPv4 parsing

--- ch24-parser-combinators/src/chex/IpParse.hs
+++ ch24-parser-combinators/src/chex/IpParse.hs
@@ -2,6 +2,7 @@
 
 module IpParse where
 
+import Data.Bits
 import Data.Word
 import Text.Trifecta
 import Control.Applicative
@@ -14,9 +15,21 @@ import Control.Applicative
 --
 newtype IPv4Address =
   IPv4Address Word32
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord)
 
--- | Parses a number that is in range (0-255)
+-- | Renders IPv4 address as string.
+instance Show IPv4Address where
+  show (IPv4Address v) =
+    concat [ show $ shiftR v 24 .&. 255
+           , "."
+           , show $ shiftR v 16 .&. 255
+           , "."
+           , show $ shiftR v 8  .&. 255
+           , "."
+           , show $ v .&. 255
+           ]
+
+-- | Parses a natural number from range 0-255.
 parseByte :: Parser Word32
 parseByte = do
   n <- natural
@@ -28,4 +41,16 @@ parseByte = do
                | n > 255   = Nothing
                | otherwise = Just $ fromIntegral n
 
-main = parseString parseByte mempty "255"  
+-- | Parses IPv4 addresss
+parseIPv4 :: Parser IPv4Address
+parseIPv4 = do
+  b1 <- flip shiftL 24 <$> parseByte
+  char '.'
+  b2 <- flip shiftL 16 <$> parseByte
+  char '.'
+  b3 <- flip shiftL 8 <$> parseByte
+  char '.'
+  b4 <- parseByte
+  return $ IPv4Address $ b1 .|. b2 .|. b3 .|. b4
+
+main = parseString parseIPv4 mempty "172.16.254.1"  
