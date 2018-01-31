94deb5b41aaf803868c694c3b8af5e00740cf84c
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-01-30 21:53:58 -0800

    composing-types: identity and compose Functor instances

--- ch25-composing-types/src/FunctionsAsTypes.hs
+++ ch25-composing-types/src/FunctionsAsTypes.hs
@@ -22,3 +22,29 @@ newtype Compose f g a =
 -- > let xs = [Just (1::Int), Nothing]
 -- > :t Compose xs
 -- Compose [Just (1::Int), Nothing] :: Compose [] Maybe Int
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose m) = Compose $ (fmap . fmap) f m
+
+
+-- One less bit of structure
+newtype One f a =
+  One (f a)
+  deriving (Eq, Show)
+
+instance Functor f => Functor (One f) where
+  fmap f (One m) = One $ fmap f m
+
+-- One more layer of structure
+newtype Three f g h a =
+  Three (f (g (h a)))
+  deriving (Eq, Show)
+
+instance (Functor f, Functor g, Functor h) => Functor (Three f g h) where
+  fmap f (Three m) = Three $ (fmap . fmap . fmap) f m
+
+v :: Compose [] Maybe (Compose Maybe [] Integer)
+v = Compose [Just (Compose $ Just [1])]
