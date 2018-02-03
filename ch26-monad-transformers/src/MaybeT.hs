8ec84ffb2c4327375600a427ea86422c02c73ff6
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-03 15:59:24 -0800

    monad transformers

---
+++ ch26-monad-transformers/src/MaybeT.hs
@@ -0,0 +1,26 @@
+module MaybeT where
+
+newtype MaybeT m a =
+  MaybeT { runMaybeT :: m (Maybe a) }
+
+{-
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+-}
+-- Nothing special here
+instance Functor m => Functor (MaybeT m) where
+  fmap f (MaybeT m) = MaybeT $ (fmap . fmap) f m
+
+{-
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure x = Compose $ pure $ pure x
+  (Compose f) <*> (Compose v) = Compose $ fmap (<*>) f <*> v
+-}
+-- Same as compose
+instance Applicative m => Applicative (MaybeT m) where
+  pure = MaybeT . pure . Just
+  (MaybeT f) <*> (MaybeT v) = MaybeT $ fmap (<*>) f <*> v
+
