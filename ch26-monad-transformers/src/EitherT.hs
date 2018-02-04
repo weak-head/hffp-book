e49d1694612f64c063e279eacca1686706b8f0e3
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-03 16:55:59 -0800

    EitherT: functor, applicative, monad

---
+++ ch26-monad-transformers/src/EitherT.hs
@@ -0,0 +1,20 @@
+module EitherT where
+
+newtype EitherT e m a =
+  EitherT { runEitherT :: m (Either e a) }
+
+instance Functor m => Functor (EitherT e m) where
+  fmap f (EitherT m) = EitherT $ (fmap . fmap) f m
+
+instance Applicative m => Applicative (EitherT e m) where
+  pure = EitherT . pure . pure
+  (EitherT f) <*> (EitherT v) = EitherT $ (<*>) <$> f <*> v
+
+instance Monad m => Monad (EitherT e m) where
+  return = pure
+  (EitherT m) >>= f =
+    EitherT $ do
+      v <- m
+      case v of
+        Left  e -> return $ Left e
+        Right a -> runEitherT $ f a
