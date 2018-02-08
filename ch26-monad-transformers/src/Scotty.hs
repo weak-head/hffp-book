179490926b6e223276390b0b4d875d10eb17ef92
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-08 11:06:24 -0800

    scotty: lift

---
+++ ch26-monad-transformers/src/Scotty.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+{-
+
+newtype ScottyT e m a =
+  ScottyT { runS :: State (ScottyState e m) a }
+  deriving (Functor, Applicative, Monad)
+
+newtype ActionT e m a =
+  ActionT { runAM :: ExceptT (ActionError e) (ReaderT ActionEnv (StateT ScottyResponse m)) a }
+  deriving ( Functor, Applicative )
+
+type ScottyM = ScottyT Text IO
+type ActionM = ActionT Text IO
+
+-}
+
+module Scotty where
+
+import Web.Scotty
+import Data.Monoid (mconcat)
+import Control.Monad.Trans.Class (lift)
+
+main = scotty 3000 $ do
+  get "/:word" $ do
+    beam <- param "word"
+    lift $ putStrLn "hello"
+    html $ mconcat [ "<h1>Scotty, "
+                   , beam
+                   , " me up!</h1>"]
