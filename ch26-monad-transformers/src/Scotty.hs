f0112171d8cae0206e8e1ea8c10d821ad0420f58
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-08 12:01:49 -0800

    ActionT lift

--- ch26-monad-transformers/src/Scotty.hs
+++ ch26-monad-transformers/src/Scotty.hs
@@ -18,13 +18,27 @@ type ActionM = ActionT Text IO
 module Scotty where
 
 import Web.Scotty
+import Web.Scotty.Internal.Types (ActionT(..))
 import Data.Monoid (mconcat)
 import Control.Monad.Trans.Class (lift)
 
 main = scotty 3000 $ do
   get "/:word" $ do
     beam <- param "word"
-    lift $ putStrLn "hello"
+--    lift $ putStrLn "hello"
+    ActionT . lift . lift . lift $ putStrLn "hello"
     html $ mconcat [ "<h1>Scotty, "
                    , beam
                    , " me up!</h1>"]
+
+{-
+lift :: (Monad m, MonadTrans t) => m a -> t m a
+lift :: (MonadTrans t) => IO a -> t IO a
+lift :: IO a -> ActionM a
+lift :: IO () -> ActionM ()
+
+-- three lifts, one each for ExceptT, ReaderT and StateT.
+instance MonadTrans (ActionT e) where
+  lift = ActionT . lift . lift . lift
+
+-}
