4558b018dacad3d96b35bdb944ff8ba07b4fad0b
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-08 13:11:56 -0800

    scotty, lifting

--- ch26-monad-transformers/src/Scotty.hs
+++ ch26-monad-transformers/src/Scotty.hs
@@ -17,16 +17,41 @@ type ActionM = ActionT Text IO
 
 module Scotty where
 
+import Control.Monad  (liftM)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State.Lazy hiding (get)
+import Data.Monoid (mconcat)
 import Web.Scotty
 import Web.Scotty.Internal.Types (ActionT(..))
-import Data.Monoid (mconcat)
-import Control.Monad.Trans.Class (lift)
 
 main = scotty 3000 $ do
   get "/:word" $ do
     beam <- param "word"
+--
 --    lift $ putStrLn "hello"
-    ActionT . lift . lift . lift $ putStrLn "hello"
+--
+--    ActionT . lift . lift . lift $ putStrLn "hello"
+--
+--    (ActionT
+--      . (ExceptT . liftM Right)
+--      . lift
+--      . lift) (putStrLn "hello")
+--
+--    (ActionT
+--      . ExceptT . liftM Right
+--      . ReaderT . const
+--      . lift) (putStrLn "hello")
+--
+    (ActionT
+      . ExceptT . liftM Right
+      . ReaderT . const
+      . \m -> StateT (\s -> do
+                         a <- m
+                         return (a, s))
+      ) (putStrLn "hello")
+
     html $ mconcat [ "<h1>Scotty, "
                    , beam
                    , " me up!</h1>"]
@@ -40,5 +65,19 @@ lift :: IO () -> ActionM ()
 -- three lifts, one each for ExceptT, ReaderT and StateT.
 instance MonadTrans (ActionT e) where
   lift = ActionT . lift . lift . lift
+
+instance MonadTrans (ExceptT e) where
+  lift = ExceptT . liftM Right
+
+instance MonadTrans (ReaderT r) where
+  lift = liftReaderT
+
+liftReaderT :: m a -> ReaderT r m a
+liftReaderT m = ReaderT (const m)
+
+instance MonadTrans (StateT s) where
+  lift m = StateT $ \s -> do
+      a <- m
+      return (a, s)
 
 -}
