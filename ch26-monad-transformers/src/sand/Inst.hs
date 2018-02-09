aa5212a4557d23010d87e0d82ab57565236bfbf0
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-09 14:42:49 -0800

    Inst: MaybeT

--- ch26-monad-transformers/src/sand/Inst.hs
+++ ch26-monad-transformers/src/sand/Inst.hs
@@ -5,6 +5,18 @@ module Inst where
 newtype MaybeT m a =
   MaybeT { runMaybeT :: m (Maybe a) }
 
+instance Functor m => Functor (MaybeT m) where
+  fmap f (MaybeT m) = MaybeT $ (fmap . fmap) f m
+
+instance Applicative m => Applicative (MaybeT m) where
+  pure = MaybeT . pure . pure
+  (MaybeT f) <*> (MaybeT m) =
+    MaybeT $ fmap (<*>) f <*> m
+
+instance Monad m => Monad (MaybeT m) where
+  return = pure
+  (MaybeT m) >>= f =
+    MaybeT $ m >>= maybe (return Nothing) (runMaybeT . f)
 
 ----------------------------------------------------------------------
 
