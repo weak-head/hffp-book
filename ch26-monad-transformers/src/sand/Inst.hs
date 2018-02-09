107612c190844ede5f1d7853c049a8a4b2c97041
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-09 15:18:55 -0800

    Inst: ReaderT

--- ch26-monad-transformers/src/sand/Inst.hs
+++ ch26-monad-transformers/src/sand/Inst.hs
@@ -11,7 +11,7 @@ instance Functor m => Functor (MaybeT m) where
 instance Applicative m => Applicative (MaybeT m) where
   pure = MaybeT . pure . pure
   (MaybeT f) <*> (MaybeT m) =
-    MaybeT $ fmap (<*>) f <*> m
+    MaybeT $ (<*>) <$> f <*> m
 
 instance Monad m => Monad (MaybeT m) where
   return = pure
@@ -29,10 +29,33 @@ instance Functor m => Functor (ExceptT e m) where
 instance Applicative m => Applicative (ExceptT e m) where
   pure = ExceptT . pure . pure
   (ExceptT f) <*> (ExceptT m) =
-    ExceptT $ fmap (<*>) f <*> m
+    ExceptT $ (<*>) <$> f <*> m
 
 instance Monad m => Monad (ExceptT e m) where
   return = pure
   (ExceptT m) >>= f =
     ExceptT $ m >>= either (return . Left) (runExceptT . f)
-    
+
+----------------------------------------------------------------------
+
+newtype ReaderT r m a =
+  ReaderT { runReaderT :: r -> m a }
+
+instance Functor m => Functor (ReaderT r m) where
+  fmap f (ReaderT m) = ReaderT $ (fmap . fmap) f m
+
+instance Applicative m => Applicative (ReaderT r m) where
+  pure = ReaderT . pure . pure
+  (ReaderT f) <*> (ReaderT m) =
+    ReaderT $ (<*>) <$> f <*> m
+
+instance Monad m => Monad (ReaderT r m) where
+  return = pure
+  (ReaderT m) >>= f =
+    ReaderT $ \r ->
+      m r >>= flip runReaderT r . f
+
+----------------------------------------------------------------------
+
+newtype StateT s m a =
+  StateT { runStateT :: s -> m (a, s) }
