488c02bf176d865ec884366e969a6eb06cde9951
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-09 18:18:45 -0800

    MonadTrans: intances

--- ch26-monad-transformers/src/MonadTrans.hs
+++ ch26-monad-transformers/src/MonadTrans.hs
@@ -1,5 +1,11 @@
 module MonadTrans where
 
+import Control.Monad (liftM)
+import EitherT
+import ReaderT
+import StateT
+import MaybeT
+
 {-
 
 fmap :: Functor f =>      (a -> b) -> f a -> f b
@@ -10,9 +16,32 @@ liftM :: Monad m =>       (a -> b) -> m a -> m b
 
 -}
 
-{-
-
 class MonadTrans t where
-  lift : : (Monad m) => m a -> t m a
+  lift :: (Monad m) => m a -> t m a
 
--}
+----------------------------------------------------------------------
+
+newtype IdentityT m a = IdentityT { runIdentityT :: m a }
+
+instance MonadTrans IdentityT where
+  lift = IdentityT
+
+----------------------------------------------------------------------
+
+instance MonadTrans MaybeT where
+  lift = MaybeT . liftM Just
+
+----------------------------------------------------------------------
+
+instance MonadTrans (ReaderT r) where
+  lift = ReaderT . const
+
+----------------------------------------------------------------------
+
+instance MonadTrans (EitherT e) where
+  lift = EitherT . liftM Right
+
+----------------------------------------------------------------------
+
+instance MonadTrans (StateT s) where
+  lift m = StateT $ \s -> liftM (flip (,) s) m
