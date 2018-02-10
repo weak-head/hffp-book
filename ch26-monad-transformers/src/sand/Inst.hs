1c1a0da4850719439c47606fc71cddebd036a96b
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-09 16:38:02 -0800

    Inst: StateT

--- ch26-monad-transformers/src/sand/Inst.hs
+++ ch26-monad-transformers/src/sand/Inst.hs
@@ -59,3 +59,21 @@ instance Monad m => Monad (ReaderT r m) where
 
 newtype StateT s m a =
   StateT { runStateT :: s -> m (a, s) }
+
+instance Functor m => Functor (StateT s m) where
+  fmap f (StateT m) = StateT $ fmap (fmap (fp f)) m
+    where fp f (a, b) = (f a, b)
+
+instance Monad m => Applicative (StateT s m) where
+  pure x = StateT $ \s -> pure (x, s)
+  (StateT f) <*> v =
+    StateT $ \s -> do
+      (f', s') <- f s
+      runStateT (fmap f' v) s'
+
+instance Monad m => Monad (StateT s m) where
+  return = pure
+  (StateT m) >>= f =
+    StateT $ \s -> do
+    (a, s') <- m s
+    runStateT (f a) s'
