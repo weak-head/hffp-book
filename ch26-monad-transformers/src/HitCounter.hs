e2512f9732ae6a124bbd08e189030a3e10d57e33
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-13 21:38:15 -0800

    HitCounter: basic implementation

--- ch26-monad-transformers/src/HitCounter.hs
+++ ch26-monad-transformers/src/HitCounter.hs
@@ -28,24 +28,39 @@ type Handler = ActionT Text (ReaderT Config IO)
 bumpBoomp :: Text
           -> M.Map Text Integer
           -> (M.Map Text Integer, Integer)
-bumpBoomp k m = undefined
+bumpBoomp k m = let mp = M.alter put k m
+                in (mp, mp M.! k)
+  where put Nothing  = Just 1
+        put (Just n) = Just $ n + 1
 
+-- ScottyT Text (ReaderT Config IO)
 app :: Scotty ()
 app =
+
+  -- ActionT Text (ReaderT Config IO)
   get "/:key" $ do
     unprefixed <- param "key"
-    let key' = mappend undefined unprefixed
-    newInteger <- undefined
+    pref       <- lift $ ReaderT $ \r -> return $ prefix r
+
+    let key' = mappend pref unprefixed
+
+    newInteger <- lift $ ReaderT $ \r -> do
+      m <- readIORef $ counts r
+      let (m', cnt) = bumpBoomp key' m
+      writeIORef (counts r) m'
+      return cnt
+
     html $ mconcat
       [ "<h1>Success! Count was: "
       , TL.pack $ show newInteger
       , "</h1>"
       ]
 
+-- curl localhost:3000/someString
 main :: IO ()
 main = do
   [prefixArg] <- getArgs
   counter <- newIORef M.empty
-  let config = undefined
-      runR   = undefined
+  let config = Config counter (TL.pack prefixArg)
+      runR r = runReaderT r config
   scottyT 3000 runR app
