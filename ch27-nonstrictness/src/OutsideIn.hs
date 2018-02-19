81c00f4ef7a29beb34cabd24c36da4e1b8b3a89f
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-18 16:15:23 -0800

    strict eval

--- ch27-nonstrictness/src/OutsideIn.hs
+++ ch27-nonstrictness/src/OutsideIn.hs
@@ -22,6 +22,15 @@ strictOutsideIn = do
     _    -> putStrLn "Hello"
 
 
+strictOutsideIn' :: IO ()
+strictOutsideIn' = do
+  let x :: Int
+      x = undefined
+  s <- x `seq` getLine
+  case s of
+    "hi" -> print x
+    _    -> putStrLn "Hello"
+
 ----------------------------------------------------------------------
 
 -- const
@@ -53,3 +62,20 @@ adrift =
       y = 2
       z = (x `seq` y `seq` 10, 11)
   in snd z
+
+
+----------------------------------------------------------------------
+
+ex1 = const 1 undefined         -- 1
+
+ex2 = const undefined 1         -- exception
+
+ex3 = flip const undefined 1    -- 1
+
+ex4 = flip const 1 undefined    -- exception
+
+ex5 = const undefined undefined -- exception
+
+ex6 = foldr const 'z' ['a'..'e'] -- 'a'
+
+ex7 = foldr (flip const) 'z' ['a'..'e'] -- 'z'
