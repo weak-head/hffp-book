5d362ef1d0c96bf0517510545839a1d499a865b9
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-19 17:24:08 -0800

    nonstrictness: prevent sharing

--- ch27-nonstrictness/src/Sharing.hs
+++ ch27-nonstrictness/src/Sharing.hs
@@ -72,4 +72,100 @@ sa = Just "a"
 -- returnIO (: ((Just (unpackCString# "a"#)) `cast` ...) ([]))
 
 ----------------------------------------
+--- Prevent sharing --------------------
+
+f :: a -> Int
+f _ = trace "f" 1
+-- > f 'a'
+-- f
+-- 1
+-- > f 'a'
+-- f
+-- 1
+
 ----------------------------------------
+
+ca :: Int
+ca = let a = trace "a" 2 + 2
+     in (a + a)
+-- > ca
+-- a
+-- 8
+-- > ca
+-- 8
+
+
+cf :: Int
+cf = (trace "a" 2 + 2)
+   + (trace "a" 2 + 2)
+-- > cf
+-- a
+-- a
+-- 8
+-- > cf
+-- 8
+
+----------------------------------------
+
+ff :: a -> Int
+ff = trace "f" const 1 -- eta reduction
+-- > ff 'a'
+-- f
+-- 1
+-- > ff 'a'
+-- 1
+-- > ff 'b'
+-- 1
+
+fg :: a -> Int
+fg v = trace "f" const 1 v
+-- > fg 'a'
+-- f
+-- 1
+-- > fg 'a'
+-- f
+-- 1
+-- > fg 'b'
+-- f
+-- 1
+
+----------------------------------------
+
+-- > let blah = Just 1
+-- > fmap ((+1) :: Int -> Int) blah
+-- Just 2
+--
+-- > :sprint blah
+-- blah = _
+--
+-- > :t blah
+-- blah :: Num a => Maybe a
+
+
+-- > let bl = Just 1
+-- > :t bl
+-- bl :: Num a => Maybe a
+--
+-- > :sprint bl
+-- bl = _
+--
+-- > fmap (+1) bl
+-- Just 2
+--
+-- > :sprint bl
+-- bl = _
+
+
+-- > let fm = fmap (+1) bl
+-- > :t fm
+-- fm :: Num b => Maybe b
+--
+-- > :sprint fm
+-- fm = _
+--
+-- > fm
+-- Just 2
+--
+-- :sprint fm
+-- fm = _
+
