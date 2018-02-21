292cbb84cd0686a895cdf74de940598fcfd28763
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-20 23:01:38 -0800

    basic-libraries: criterion, !??

--- ch28-basic-libraries/src/Main.hs
+++ ch28-basic-libraries/src/Main.hs
@@ -6,11 +6,27 @@ module Main where
 
 import Criterion.Main
 
+----------------------------------------
+
 infixl 9 !?
 _      !? n | n < 0 = Nothing
 []     !? _         = Nothing
 (x:_)  !? 0         = Just x
 (_:xs) !? n         = xs !? (n - 1)
+
+----------------------------------------
+
+infixl 9 !??
+{-# INLINABLE (!??) #-}
+xs !?? n
+  | n < 0     = Nothing
+  | otherwise =
+      foldr (\x r k -> case k of
+                         0 -> Just x
+                         _ -> r (k-1))
+      (const Nothing) xs n
+
+----------------------------------------
 
 myList :: [Int]
 myList = [1..9999]
@@ -19,6 +35,7 @@ myList = [1..9999]
 -- ./src/Main
 main :: IO ()
 main = defaultMain
-  [ bench "index list 9999" $ whnf (myList !!) 9998
-  , bench "index list maybe index 9999" $ whnf (myList !?) 9998
+  [ bench "index list !!  9999" $ whnf (myList !!)  9998
+  , bench "index list !?  9999" $ whnf (myList !?)  9998
+  , bench "index list !?? 9999" $ whnf (myList !??) 9998
   ]
