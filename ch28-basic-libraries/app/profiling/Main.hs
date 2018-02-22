2f8ca5dd9306bb8a028922f10093a4a1485a9861
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-21 21:53:21 -0800

    profiling: cpu usage

--- ch28-basic-libraries/app/profiling/Main.hs
+++ ch28-basic-libraries/app/profiling/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+f :: IO ()
+f = do
+  print ([1..] !! 999999)
+  putStrLn "f"
+
+g :: IO ()
+g = do
+  print ([1..] !! 9999999)
+  putStrLn "g"
+
+
+--
+-- > stack ghc -- -prof -fprof-auto -rtsopts -O2 Main.hs
+-- > ./Main +RTS -P
+-- > cat Main.prof
+--
 main :: IO ()
-main = putStrLn ""
+main = f >> g
