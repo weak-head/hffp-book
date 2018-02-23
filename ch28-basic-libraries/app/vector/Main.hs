32b869bbac085f476dad8143ef3b7f98cc7b2deb
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-23 13:54:42 -0800

    basic-libraries: vector fusion

--- ch28-basic-libraries/app/vector/Main.hs
+++ ch28-basic-libraries/app/vector/Main.hs
@@ -27,10 +27,24 @@ v = V.fromList [1..1000]
 
 ----------------------------------------
 
+testV' :: Int -> V.Vector Int
+testV' n =
+  V.map (+n) $ V.map (+n) $
+  V.map (+n) $ V.map (+n) $
+  V.fromList [1..10000]
+
+testV :: Int -> V.Vector Int
+testV n =
+  V.map ((+n) . (+n) . (+n) . (+n)) $
+  V.fromList [1..10000]
+
+----------------------------------------
+
 main :: IO ()
 main = defaultMain
-  [ bench "slicing list" $
-    whnf (head . slice 100 900) l
-  , bench "slicing vector" $
-    whnf (V.head . V.slice 100 900) v ]
+  [ bench "slicing list"   $  whnf (head . slice 100 900) l
+  , bench "slicing vector" $  whnf (V.head . V.slice 100 900) v
+  , bench "vector map prefused" $ whnf testV 9998
+  , bench "vector map will be fused" $ whnf testV' 9998
+  ]
 
