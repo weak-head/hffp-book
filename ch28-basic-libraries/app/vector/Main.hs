cfdd67c2cd98e8b04dabab2f1bb3bf758eabfce5
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-23 16:24:12 -0800

    vector: mutable vector, ST

--- ch28-basic-libraries/app/vector/Main.hs
+++ ch28-basic-libraries/app/vector/Main.hs
@@ -10,9 +10,13 @@ data Vector a =
 
 -}
 
-import Data.Vector ((//))
-import Criterion.Main
+import           Control.Monad.Primitive
+import           Control.Monad.ST
+import           Criterion.Main
+import           Data.Vector ((//))
 import qualified Data.Vector as V
+import qualified Data.Vector.Generic.Mutable as GM
+import qualified Data.Vector.Mutable as MV
 
 ----------------------------------------
 
@@ -53,17 +57,60 @@ slow n = go n vec
 batchList :: Int -> V.Vector Int
 batchList n = vec // updates
   where
-    updates = fmap (\k -> (k, 0)) [1..n]
+    updates = fmap (\k -> (k, 0)) [0..n]
+
+batchVector :: Int -> V.Vector Int
+batchVector n = V.unsafeUpdate vec updates
+  where
+    updates = fmap (\k -> (k, 0)) (V.fromList [0..n])
+
+----------------------------------------
+
+mutableUpdateIO :: Int -> IO (MV.MVector RealWorld Int)
+mutableUpdateIO n = do
+  mvec <- GM.new (n+1)
+  go n mvec
+  where
+    go 0 v = return v
+    go n v = MV.write v n 0 >> go (n-1) v
+
+mutableUpdateST :: Int -> V.Vector Int
+mutableUpdateST n = runST $ do
+  mvec <- GM.new (n+1)
+  go n mvec
+  where
+    go 0 v = V.freeze v
+    go n v = MV.write v n  0 >> go (n-1) v
+
+----------------------------------------
 
 main :: IO ()
 main = defaultMain
-  [ bench "slicing list"   $  whnf (head . slice 100 900) l
-  , bench "slicing vector" $  whnf (V.head . V.slice 100 900) v
-  ---
-  , bench "vector map prefused" $ whnf testV 9998
-  , bench "vector map will be fused" $ whnf testV' 9998
-  ---
-  , bench "slow" $ whnf slow 9998
-  , bench "batch list" $ whnf batchList 9998
-  ]
+  [
+    bgroup "Slicing" [
+        bench "slicing list"   $  whnf (head . slice 100 900) l
+      , bench "slicing vector" $  whnf (V.head . V.slice 100 900) v
+      ]
 
+  ---
+
+  , bgroup "Stream fusion" [
+        bench "vector map prefused" $ whnf testV 9998
+      , bench "vector map will be fused" $ whnf testV' 9998
+      ]
+
+  ---
+
+  , bgroup "Updates" [
+        bgroup "Persistent vectors" [
+            bench "slow" $ whnf slow 9998
+          , bench "batch list" $ whnf batchList 9998
+          , bench "batch vector" $ whnf batchVector 9998
+          ]
+      , bgroup "Mutable vectors" [
+            bench "mutable IO vector" $ whnfIO $ mutableUpdateIO 9998
+          , bench "mutable ST vector" $ whnf mutableUpdateST 9998
+          ]
+      ]
+
+  ]
