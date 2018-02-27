7c57b6ef2f443391522af9ab60047b7bcb79a89b
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-26 21:22:41 -0800

    basic-libraries: queue tests

--- ch28-basic-libraries/src/Queue.hs
+++ ch28-basic-libraries/src/Queue.hs
@@ -1,5 +1,6 @@
 module Queue where
 
+import Data.Maybe (fromJust)
 import Criterion.Main
 
 ----------------------------------------
@@ -85,6 +86,64 @@ sequantialQueueV2 n =
       Just (_, nq) -> unwrap nq
       Nothing      -> Queue [] []
 
+----
+
+alteringQueuePlain :: Int -> QueuePlain Int
+alteringQueuePlain n =
+  foldr pushPop (QueuePlain []) [1..n]
+  where pushPop a q =
+          let q' = pushPlain a q
+          in snd $ fromJust $ popPlain q'
+
+alteringQueueV1 :: Int -> Queue Int
+alteringQueueV1 n =
+  foldr pushPop (Queue [] []) [1..n]
+  where pushPop a q =
+          let q' = push a q
+          in snd $ fromJust $ pop q'
+
+alteringQueueV2 :: Int -> Queue Int
+alteringQueueV2 n =
+  foldr pushPop (Queue [] []) [1..n]
+  where pushPop a q =
+          let q' = push' a q
+          in snd $ fromJust $ pop' q'
+
+----
+
+alteringSeriesPlain :: Int -> QueuePlain Int
+alteringSeriesPlain n =
+  foldr pushPop (QueuePlain []) [1..n]
+  where pushPop a q =
+          let q' = pushPlainN 5 a q
+          in unwrap q'
+        pushPlainN n a q = foldr pushPlain q (replicate n a)
+        unwrap q = case popPlain q of
+          Just (_, nq) -> unwrap nq
+          Nothing      -> QueuePlain []
+
+alteringSeriesV1 :: Int -> Queue Int
+alteringSeriesV1 n =
+  foldr pushPop (Queue [] []) [1..n]
+  where pushPop a q =
+          let q' = pushPlainN 5 a q
+          in unwrap q'
+        pushPlainN n a q = foldr push q (replicate n a)
+        unwrap q = case pop q of
+          Just (_, nq) -> unwrap nq
+          Nothing      -> Queue [] []
+
+alteringSeriesV2 :: Int -> Queue Int
+alteringSeriesV2 n =
+  foldr pushPop (Queue [] []) [1..n]
+  where pushPop a q =
+          let q' = pushPlainN 5 a q
+          in unwrap q'
+        pushPlainN n a q = foldr push' q (replicate n a)
+        unwrap q = case pop' q of
+          Just (_, nq) -> unwrap nq
+          Nothing      -> Queue [] []
+
 ----------------------------------------
 
 -- > stack build --ghc-options -O2
@@ -94,4 +153,12 @@ runBench  = defaultMain
   [ bench "QueuePlain sequential test" $ whnf sequantialQueuePlain 9999
   , bench "QueueV1    sequential test" $ whnf sequantialQueueV1 9999
   , bench "QueueV2    sequential test" $ whnf sequantialQueueV2 9999
+  ---
+  , bench "QueuePlain altering test" $ whnf alteringQueuePlain 9999
+  , bench "QueueV1    altering test" $ whnf alteringQueueV1 9999
+  , bench "QueueV2    altering test" $ whnf alteringQueueV2 9999
+  ---
+  , bench "QueuePlain series test" $ whnf alteringSeriesPlain 2555
+  , bench "QueueV1    series test" $ whnf alteringSeriesV1 2555
+  , bench "QueueV2    series test" $ whnf alteringSeriesV2 2555
   ]
