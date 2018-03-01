3573c91a4ec6f0c826e81c487bc9ab1d2cb8c153
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-02-28 23:15:29 -0800

    exception: handling multiple related exceptions

--- ch30-when-things-go-wrong/src/MakingOwnExceptions.hs
+++ ch30-when-things-go-wrong/src/MakingOwnExceptions.hs
@@ -43,7 +43,10 @@ catchBoth ioInt =
   [ Handler (\(NotEven n) -> return maxBound)
   , Handler (\(NotDivThree n) -> return minBound)
   ]
-
+{-
+data Handler a where
+  Handler :: Exception e => (e -> IO a) -> Handler a
+-}
 
 ex1 :: IO ()
 ex1 = mapM_ exec [1..12]
@@ -51,3 +54,25 @@ ex1 = mapM_ exec [1..12]
     exec f = catchBoth (evenAndThreeDiv f) >>= print
 
 ----------------------------------------
+----------------------------------------
+
+data EATD =
+    NotEven' Int
+  | NotDivideThree' Int
+  deriving (Eq, Show)
+
+instance Exception EATD
+
+evenAndDivThree' :: Int -> IO Int
+evenAndDivThree' n
+  | rem n 3 /= 0 = throwIO $ NotDivideThree' n
+  | even n       = throwIO $ NotEven' n
+  | otherwise    = return n
+
+type EA e = IO (Either e Int)
+
+ex2 :: IO ()
+ex2 = mapM_ exec [1..12]
+  where
+    exec n = (try $ evenAndDivThree' n :: EA EATD) >>= print
+
