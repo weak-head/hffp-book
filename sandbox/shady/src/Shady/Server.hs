f36a036f799b647d1a9a285e6c5ead44e074156f
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-03-07 17:11:10 -0800

    reader for server

--- sandbox/shady/src/Shady/Server.hs
+++ sandbox/shady/src/Shady/Server.hs
@@ -5,37 +5,50 @@ module Shady.Server
   )
 where
 
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.Reader
 import           Control.Concurrent
 import           Control.Monad
 import           Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import           Network.Socket
 import qualified Network.Socket.ByteString as NBS
 
-type ConnectionInfo = String
-type Port           = Int
+----------------------------------------
+
+-- | Contains the information about the socket and
+-- database connection.
+data ConnectionInfo =
+  ConnectionInfo { dbInfo   :: DbConnInfo
+                 , sockInfo :: Socket }
+  deriving (Eq, Show)
+
+type Handler    = ReaderT ConnectionInfo IO ()
+type DbConnInfo = String
+type Port       = Int
+
+----------------------------------------
 
 -- | Start shady server.
-startServer :: ConnectionInfo -> Port -> IO ()
+startServer :: DbConnInfo -> Port -> IO ()
 startServer con port = do
   addr <- getAddress port
   sock <- socket (addrFamily addr) Stream defaultProtocol
   bind sock (addrAddress addr)
   listen sock 5
-  handleClients con sock
+  runReaderT handleClients (ConnectionInfo con sock)
   close sock
 
 -- | Handle connections from clients.
-handleClients :: ConnectionInfo -> Socket -> IO ()
-handleClients con sock = forever $ do
-  (soc, _) <- accept sock
-  forkIO $ void $ handleClient con soc
+handleClients :: Handler
+handleClients = ReaderT $ \ci -> forever $ do
+  (soc, _) <- accept (sockInfo ci)
+  forkIO $ runReaderT handleClient (ci { sockInfo = soc }) >> close soc
 
 -- | Handle single client.
-handleClient :: ConnectionInfo -> Socket -> IO ()
-handleClient con sock = do
-  cmd <- NBS.recv sock 1024
+handleClient :: Handler
+handleClient = ReaderT $ \ci -> do
+  cmd <- NBS.recv (sockInfo ci) 1024
   print $ decodeUtf8 cmd
-  close sock
 
 -- | Gets address to bind the socket.
 getAddress :: Port -> IO AddrInfo
