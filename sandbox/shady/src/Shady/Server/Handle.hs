fb5cf2c7b264848895c396c7a69d605f0e3dd5ff
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-03-13 17:47:26 -0700

    registration: responding back to client. Hanling exception in RSWT monad.

--- sandbox/shady/src/Shady/Server/Handle.hs
+++ sandbox/shady/src/Shady/Server/Handle.hs
@@ -15,6 +15,7 @@ where
 
 import           Control.Exception hiding ( handle )
 import           Control.Monad ( void )
+import qualified Control.Monad.Catch as MC
 import           Control.Monad.IO.Class ( liftIO )
 import           Control.Monad.Loops ( iterateWhile )
 import           Control.Monad.Trans.Class ( lift )
@@ -61,6 +62,80 @@ handleClient = void $ iterateWhile isAlive $ do
   readCommand >>= handleCommand
   get
 
+handleCommand :: Either String Command -> ClientHandler ()
+handleCommand eth =
+  case eth of
+    Left err  -> parsingErrorHandler err
+    Right cmd -> handle cmd
+
+handle :: Command -> ClientHandler ()
+handle (Register userName) = registerHandler userName
+handle (Login userName)    = loginHandler userName
+handle Logout              = logoutHandler
+handle (Read from)         = readHandler from
+handle (Send to message)   = sendHandler to message
+
+--- Command Handlers -------------------
+
+-- | Parse error handler.
+parsingErrorHandler :: String -> ClientHandler ()
+parsingErrorHandler _ = do
+  let msg = "Failed to parse the command.\n"
+  respondLogError msg
+
+-- | Register a new user.
+registerHandler :: String -> ClientHandler ()
+registerHandler userName = do
+  ei <- ask
+
+  let
+      dbCon      = getDatabaseCon ei
+      createUser = do
+        liftIO $ withConnection dbCon (DB.createUser userName)
+        respondClient $ concat [ "<"
+                               , userName
+                               , "> account has been created and is ready to use.\n"
+                               ]
+
+  createUser `MC.catch` handleEx
+
+  where
+    handleEx :: ItemAlreadyExistsException -> ClientHandler ()
+    handleEx ex = do
+      let
+          msg = concat [ "Unable to create the user. The name <"
+                       , itemName ex
+                       , "> is already being used.\n"
+                       ]
+      respondLogError msg
+
+loginHandler :: String -> ClientHandler ()
+loginHandler = undefined
+
+logoutHandler :: ClientHandler ()
+logoutHandler = modify $ \cs -> cs { isAlive = False }
+
+readHandler :: String -> ClientHandler ()
+readHandler from = undefined
+
+sendHandler :: String -> String -> ClientHandler ()
+sendHandler to msg = undefined
+
+----------------------------------------
+
+-- | Responds to client with the error message that is being logged.
+respondLogError :: String -> ClientHandler ()
+respondLogError msg = do
+  cs <- get
+  writeLog msg
+  writeSocket (getSock cs) msg
+
+-- | Respond to client with general message.
+respondClient :: String -> ClientHandler ()
+respondClient msg = do
+  cs <- get
+  writeSocket (getSock cs) msg
+
 ----------------------------------------
 
 -- | Gets input from client and parses into command.
@@ -76,51 +151,6 @@ readSocket sock = liftIO $ NBS.recv sock 1024
 -- | Writes string to socket.
 writeSocket :: Socket -> String -> ClientHandler ()
 writeSocket sock msg = liftIO $ NBS.sendAll sock (BSC.pack msg)
-
-----------------------------------------
-
-handleCommand :: Either String Command -> ClientHandler ()
-handleCommand eth =
-  case eth of
-    Left err  -> parsingErrorHandler err
-    Right cmd -> handle cmd
-
-handle :: Command -> ClientHandler ()
-handle (Register userName) = registerHandler userName
-handle (Login userName)    = loginHandler userName
-handle Logout              = logoutHandler
-handle (Read from)         = readHandler from
-handle (Send to message)   = sendHandler to message
-
--- | Parse error handler.
-parsingErrorHandler :: String -> ClientHandler ()
-parsingErrorHandler errMsg = do
-  let msg = "Failed to parse the command.\n"
-  cs <- get
-  writeLog msg
-  writeSocket (getSock cs) msg
-
--- | Register a new user.
-registerHandler :: String -> ClientHandler ()
-registerHandler userName = do
-  ei <- ask
-  let dbCon = getDatabaseCon ei
-  liftIO $ withConnection dbCon (DB.createUser userName) `catch` handleEx 
-  where
-    handleEx :: ItemAlreadyExistsException -> IO ()
-    handleEx = print
-
-loginHandler :: String -> ClientHandler ()
-loginHandler = undefined
-
-logoutHandler :: ClientHandler ()
-logoutHandler = modify $ \cs -> cs { isAlive = False }
-
-readHandler :: String -> ClientHandler ()
-readHandler from = undefined
-
-sendHandler :: String -> String -> ClientHandler ()
-sendHandler to msg = undefined
 
 ----------------------------------------
 
